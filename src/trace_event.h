be1f4ccd293a51616296d0c56575d76d8281bda8
Reid Atcheson <reid.atcheson@gmail.com>
2025-09-07 14:09:57 -0400

    remove lazy event type

--- src/trace_event.h
+++ src/trace_event.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <cstddef>
-#include <optional>
 
 #include "trace_registry.h"
 
@@ -21,32 +20,6 @@
           {FIELDS(SIMPLETRACE_FIELD_LAYOUT)}};                                 \
       return s;                                                                \
     }                                                                          \
-    static ::simpletrace::event_id_t event_id() {                              \
-      static std::optional<::simpletrace::event_id_t> cached;                  \
-      if (!cached) {                                                           \
-        cached =                                                               \
-            ::simpletrace::trace_registry_t::instance().register_type<name>(); \
-      }                                                                        \
-      return *cached;                                                          \
-    }                                                                          \
-  };
-
-#define SIMPLETRACE_EVENT_STRUCT_STATIC(name, FIELDS)                          \
-  struct name {                                                                \
-    FIELDS(SIMPLETRACE_FIELD_DECL)                                             \
-    static ::simpletrace::schema_t schema() {                                  \
-      using __trace_this_t = name;                                             \
-      static const ::simpletrace::schema_t s{                                  \
-          #name,                                                               \
-          0,                                                                   \
-          sizeof(__trace_this_t),                                              \
-          alignof(__trace_this_t),                                             \
-          {FIELDS(SIMPLETRACE_FIELD_LAYOUT)}};                                 \
-      return s;                                                                \
-    }                                                                          \
     inline static const ::simpletrace::event_id_t event_id =                   \
         ::simpletrace::trace_registry_t::instance().register_type<name>();     \
   };
-
-
-
