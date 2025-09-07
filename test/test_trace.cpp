8456800b38cfd368b3480267d74341aaa0ee7747
Reid Atcheson <reid.atcheson@gmail.com>
2025-09-07 08:20:49 -0400

    Add macros for trace event registration with cached IDs

--- test/test_trace.cpp
+++ test/test_trace.cpp
@@ -1,4 +1,26 @@
 #include "acutest.h"
+#include "trace_event.h"
 
-void test_test() {}
-TEST_LIST = {{"test_test", test_test}, {NULL, NULL}};
+using namespace simpletrace;
+
+#define EVENT_A_FIELDS(X) X(int32_t, value, dtype_t::i32)
+
+SIMPLETRACE_EVENT_STRUCT(event_a_t, EVENT_A_FIELDS)
+
+#define EVENT_B_FIELDS(X) X(int64_t, timestamp, dtype_t::timestamp)
+
+SIMPLETRACE_EVENT_STRUCT_STATIC(event_b_t, EVENT_B_FIELDS)
+
+void test_event_lazy() {
+  event_id_t id1 = event_a_t::event_id();
+  TEST_CHECK(id1 == event_a_t::event_id());
+}
+
+void test_event_eager() {
+  event_id_t id1 = event_b_t::event_id();
+  TEST_CHECK(id1 == event_b_t::event_id());
+}
+
+TEST_LIST = {{"test_event_lazy", test_event_lazy},
+             {"test_event_eager", test_event_eager},
+             {NULL, NULL}};
